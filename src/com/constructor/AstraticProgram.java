ff2d92353207386d99089e00dae1ba00e630e10a
Ajiket97 <ajitshingnare02@gmail.com>
2024-04-29 12:58:03 +0530

    new topic add

--- src/com/constructor/AstraticProgram.java
+++ src/com/constructor/AstraticProgram.java
@@ -1,5 +1,12 @@
 package com.constructor;
 
+import java.util.Iterator;
+
 public class AstraticProgram {
+	public static void main(String[] args) {
+		  for (int i = 0; i < args.length; i++) {
+			
+		}
+	}
 
 }
